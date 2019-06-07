49ce4061a650fdc529e0270ec6489db486b7248f
amlane <twelvexstring@gmail.com>
2019-06-07 11:25:10 -0500

    end points built and I have projectsById but cannot get them to remove the actions description out of the main project object

--- api/model.js
+++ api/model.js
@@ -3,3 +3,46 @@ const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
 const db = knex(knexConfig.development);
+
+module.exports = {
+    addProject,
+    addAction, 
+    getProjectById,
+    getActions,
+    getProjects
+}
+
+
+function getProjects(){
+    return db('projects')
+}
+
+function addProject(project) {
+    return db('projects')
+    .insert(project)
+};
+
+function getActions(){
+    return db('actions')
+}
+
+function addAction(action) {
+    return db('actions')
+    .insert(action)
+};
+
+function getProjectById(id) {
+    return db('actions')
+      .where({ 'actions.project_id': id })
+      .first()
+
+      .join('projects', 'projects.id', 'actions.project_id')
+      .select(
+        {id: 'projects.id'},
+        {name: 'projects.name'},
+        {projectDesc: 'projects.description'},
+        {description: 'actions.description'},
+        {notes: 'actions.notes'},
+        {completed: 'actions.completed'}
+      )
+  };
--- api/router.js
+++ api/router.js
@@ -1,6 +1,64 @@
 const router = require('express').Router();
 
-const Users = require('./model.js');
+const db = require('./model.js');
+
+router.get('/projects', (req, res) => {
+    db.getProjects()
+    .then( project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+router.post('/projects', (req, res) => {
+    db.addProject(req.body)
+    .then(project => {
+        res.status(201).json(project)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+router.get('/actions', (req, res) => {
+    db.getActions()
+    .then( action => {
+        res.status(200).json(action)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+router.post('/actions', (req, res) => {
+    db.addAction(req.body)
+    .then(action => {
+        res.status(201).json(action)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+router.get('/projects/:id', (req, res) => {
+    db.getProjectById(req.params.id)
+    .then(projects => {
+        db.getActions()
+          .where({ project_id: req.params.id})
+          .then(actions => {
+            projects.actions = actions;
+            return res.status(200).json(projects);
+          });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json(err);
+      });
+});
+
 
 
 module.exports = router;
