bb38e33beafc78296d39082189a7a3d022fd0cd8
amlane <twelvexstring@gmail.com>
2019-06-07 11:48:51 -0500

    reorganized router and model file and created/tested end points for projects

--- api/model.js
+++ api/model.js
@@ -7,20 +7,60 @@ const db = knex(knexConfig.development);
 module.exports = {
     addProject,
     addAction, 
-    getProjectById,
+    getProjectWithActionsById,
     getActions,
-    getProjects
+    getProjects, 
+    editProject,
+    deleteProject,
+    getProjectById
 }
 
+// Project Helpers
 
 function getProjects(){
     return db('projects')
 }
 
+function getProjectById(id){
+    return db('projects')
+    .where({ id })
+    .first();
+};
+
 function addProject(project) {
     return db('projects')
     .insert(project)
 };
+
+function editProject(id, changes) {
+    return db('projects')
+    .where({ id })
+    .update(changes)
+};
+
+function deleteProject(id) {
+    return db('projects')
+    .where({ id })
+    .del();  //thanks for explaining why! :)
+};
+
+
+function getProjectWithActionsById(id) {
+    return db('actions')
+      .where({ 'actions.project_id': id })
+      .first()
+
+      .join('projects', 'projects.id', 'actions.project_id')
+      .select(
+        'projects.id',
+        'projects.name',
+        'projects.description',
+        'projects.completed'
+      )
+  };
+
+
+// Action helpers
 
 function getActions(){
     return db('actions')
@@ -31,15 +71,3 @@ function addAction(action) {
     .insert(action)
 };
 
-function getProjectById(id) {
-    return db('actions')
-      .where({ 'actions.project_id': id })
-      .first()
-
-      .join('projects', 'projects.id', 'actions.project_id')
-      .select(
-        'projects.id',
-        'projects.name',
-        'projects.description'
-      )
-  };
--- api/router.js
+++ api/router.js
@@ -2,8 +2,22 @@ const router = require('express').Router();
 
 const db = require('./model.js');
 
+// '/api/projects' endpoints
+
 router.get('/projects', (req, res) => {
     db.getProjects()
+    .then( project => {
+        res.status(200).json(project)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+router.get('/projects/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.getProjectById(id)
     .then( project => {
         res.status(200).json(project)
     })
@@ -21,6 +35,53 @@ router.post('/projects', (req, res) => {
         res.status(500).json(err)
     })
 });
+
+router.put('/projects/:id', (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    db.editProject(id, changes)
+    .then(updatedProject => {
+        res.status(201).json(updatedProject)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+router.delete('/projects/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.deleteProject(id)
+    .then(deleted => {
+        res.status(200).json({ message: `${deleted} project deleted.` });
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+});
+
+router.get('/project/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.getProjectWithActionsById(id)
+    .then(projects => {
+        db.getActions()
+          .where({ project_id: id })
+          .then(actions => {
+                projects.actions = actions;
+                return res.status(200).json(projects);            
+          });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json(err);
+      });
+});
+
+
+// '/api/actions' endpoints
 
 router.get('/actions', (req, res) => {
     db.getActions()
@@ -42,24 +103,25 @@ router.post('/actions', (req, res) => {
     })
 });
 
-router.get('/projects/:id', (req, res) => {
-    const id = req.params.id;
 
-    db.getProjectById(id)
-    .then(projects => {
-        db.getActions()
-          .where({ project_id: id })
-          .then(actions => {
-            projects.actions = actions;
-            return res.status(200).json(projects);
-          });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json(err);
-      });
-});
+// Custom middleware
+
+// function verifyProjectId(req, res, next){
+//     const id = req.params.id;
+
+//     db.findById(id)
+//     .then(item => {
+//         if(item){
+//             req.item = item;
+//             next();
+//         } else {
+//             res.status(404).json({ message: "User Not Found." })
+//         }
+//     })
+//     .catch(err => {
+//         res.status(500).json(err)
+//     })
+// };
 
 
 
